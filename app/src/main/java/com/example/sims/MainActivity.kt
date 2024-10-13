ccb20c9f498963cd6365fa2e068029cc65431497
rykieldc <rlouizzedc@gmail.com>
2024-10-13 21:58:34 +0800

    Add Login layout

--- app/src/main/java/com/example/sims/MainActivity.kt
+++ app/src/main/java/com/example/sims/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
