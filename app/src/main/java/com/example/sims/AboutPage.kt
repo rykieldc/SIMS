4f81ddf4de76d27e2e0d0a0aef9702b999bcca54
rykieldc <rlouizzedc@gmail.com>
2024-10-14 13:24:59 +0800

    Modify about page

--- app/src/main/java/com/example/sims/AboutPage.kt
+++ app/src/main/java/com/example/sims/AboutPage.kt
@@ -14,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.sims.ManageUsersActivity.DrawableClickSpan
 
 class AboutPage : AppCompatActivity() {
 
