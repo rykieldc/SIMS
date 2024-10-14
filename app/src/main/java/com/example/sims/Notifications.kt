292f155870773d18d62924238ff57c7f33ca61bb
rykieldc <rlouizzedc@gmail.com>
2024-10-14 13:38:24 +0800

    Modify notifications layout

--- app/src/main/java/com/example/sims/Notifications.kt
+++ app/src/main/java/com/example/sims/Notifications.kt
@@ -1,57 +1,16 @@
 package com.example.sims
 
 import android.os.Bundle
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.text.style.DynamicDrawableSpan
-import android.text.style.ImageSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 class Notifications : Fragment() {
-
-    private lateinit var header: TextView
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
-
-        header = view.findViewById(R.id.header)
-
-        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.ic_back_arrow_circle)
-        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-
-        val spannableString = SpannableString("  ${header.text}")
-        spannableString.setSpan(
-            ImageSpan(drawable!!, DynamicDrawableSpan.ALIGN_BASELINE),
-            0,
-            1,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-        spannableString.setSpan(
-            DrawableClickSpan { requireActivity().onBackPressedDispatcher.onBackPressed() },
-            0,
-            1,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        header.text = spannableString
-        header.movementMethod = LinkMovementMethod.getInstance()
-
-        return view
-    }
-
-    class DrawableClickSpan(private val clickListener: () -> Unit) : ClickableSpan() {
-        override fun onClick(widget: View) {
-            clickListener()
-        }
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
 }
