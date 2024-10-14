3eab7d77c99a82a697f74269317b4823acc911b8
shanebergado <shanebergs1@gmail.com>
2024-10-14 08:39:16 +0800

    I make the about us page layout for this application.

---
+++ app/src/main/java/com/example/sims/layout_aboutuspage.kt
@@ -0,0 +1,20 @@
+package com.example.sims
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class layout_aboutuspage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_layout_aboutuspage)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
