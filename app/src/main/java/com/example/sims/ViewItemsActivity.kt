d1e1d92538745409d11a5999db3ae6765f47b0fa
rykieldc <rlouizzedc@gmail.com>
2024-10-26 17:58:51 +0800

    Add back button

--- app/src/main/java/com/example/sims/ViewItemsActivity.kt
+++ app/src/main/java/com/example/sims/ViewItemsActivity.kt
@@ -1,9 +1,16 @@
 package com.example.sims
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.DynamicDrawableSpan
+import android.text.style.ImageSpan
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
@@ -11,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ViewItemsActivity : AppCompatActivity() {
 
+    private lateinit var header: TextView
     private var recyclerView : RecyclerView? = null
     private var recyclerViewProductAdapter : RecyclerViewProductAdapter? = null
     private var productList = mutableListOf<Product>()
@@ -20,8 +28,31 @@ class ViewItemsActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_items)
 
-        productList = ArrayList()
+        header = findViewById(R.id.header_product)
 
+        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_back_arrow_circle)
+        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+
+        val spannableString = SpannableString("  ${header.text}")
+        spannableString.setSpan(
+            ImageSpan(drawable!!, DynamicDrawableSpan.ALIGN_BASELINE),
+            0,
+            1,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            ViewItemDetailsActivity.DrawableClickSpan { onBackPressedDispatcher.onBackPressed() },
+            0,
+            1,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        header.text = spannableString
+        header.movementMethod = LinkMovementMethod.getInstance()
+
+
+
+        productList = ArrayList()
 
         recyclerView = findViewById<View>(R.id.rvViewItems) as RecyclerView
         recyclerViewProductAdapter = RecyclerViewProductAdapter(this@ViewItemsActivity, productList)
